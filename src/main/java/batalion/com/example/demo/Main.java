afe4d2a9e789842bca42098b1919194073c7fae8
Alice Pazitor <alice.pazitor02@e-uvt.ro>
2023-11-23 11:00:53 +0200

    Strategy Pattern Implemented

--- src/main/java/batalion/com/example/demo/Main.java
+++ src/main/java/batalion/com/example/demo/Main.java
@@ -1,14 +1,20 @@
 // Main.java
 package batalion.com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        // utilizare proxy
+        SpringApplication.run(Main.class, args);
+
+        // Using Proxy Pattern
         Creatura proxyGnom = new ProxyCreatura("Gimli");
         Creatura proxyElfi = new ProxyCreatura("Legolas");
         Creatura proxyEnt = new ProxyCreatura("Treebeard");
 
-        // Creare batalioane
+        // Creating battalions
         Batalion batalion1 = new Batalion("Batalion1");
         batalion1.adaugaCreatura(proxyGnom);
         batalion1.adaugaCreatura(proxyElfi);
@@ -22,18 +28,11 @@ public class Main {
         armata.adaugaCreatura(batalion1);
         armata.adaugaCreatura(batalion2);
 
-        // Setarea dinamica a startegiei armatei
+        // Setting the strategy dynamically
         armata.setStrategy(new OffensiveStrategy());
-        armata.executeStrategy(); // Executare strategie
-		
+        armata.executeStrategy(); // Execute the strategy
 
-        // Afisare structura aramata
+        // Displaying the army structure
         armata.afisare();
-
-		// Setarea dinamica a startegiei armatei
-        armata.setStrategy(new DefensiveStrategy());
-        armata.executeStrategy(); // Executare strategie
-
-
     }
 }
